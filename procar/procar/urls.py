836da804e607bc83bc38d8b398f5a6c85d898ea9
aswanth2002-web <aswaa2918@gmail.com>
2026-03-04 11:45:45 +0530

    added

--- procar/procar/urls.py
+++ procar/procar/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.shortcuts import redirect
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', lambda request: redirect('/appcar/')),
     path('admin/', admin.site.urls),
     path('appcar/',include('appcar.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
